61545a4634c9eb2af4df1f7f9fedc936db4ada9d
vanstee <vanstee@apache.org>
2020-05-20 13:10:55 -0400

    fixes

--- FastAI/start_jupyter.sh
+++ FastAI/start_jupyter.sh
@@ -1,3 +1,3 @@
 [[ -d aicoc-ai-immersion ]] || git clone git@github.ibm.com:vanstee/aicoc-ai-immersion.git
-nohup jupyter notebook --ip=0.0.0.0 --allow-root --port=5050 --no-browser --NotebookApp.token='aicoc' --NotebookApp.password='' &
+jupyter notebook --ip=0.0.0.0 --allow-root --port=5050 --no-browser --NotebookApp.token='aicoc' --NotebookApp.password='' &
 

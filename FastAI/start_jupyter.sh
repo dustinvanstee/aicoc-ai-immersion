db0ab1e788946f4531ffe22f4a4eb32bb1e58c59
vanstee <vanstee@apache.org>
2020-05-20 22:25:25 -0400

    add readme

--- FastAI/start_jupyter.sh
+++ FastAI/start_jupyter.sh
@@ -1,3 +1,5 @@
 [[ -d aicoc-ai-immersion ]] || git clone git@github.ibm.com:vanstee/aicoc-ai-immersion.git
+cd aicoc-ai-immersion
+git pull origin master
 nohup jupyter notebook --ip=0.0.0.0 --allow-root --port=5050 --no-browser --NotebookApp.token='aicoc' --NotebookApp.password='' &
 

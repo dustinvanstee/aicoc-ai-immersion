ba4e6c3c4a3350301df49645329e89cf2cfe4d21
Dustin VanStee <vanstee@login5.summit.olcf.ornl.gov>
2020-09-22 11:25:52 -0400

    summit fixes:

--- process_files.sh
+++ process_files.sh
@@ -1,3 +1,5 @@
 sed -i s/fillmein/Rapids/g settings.ini
 nbdev_build_lib
-sed -i s/Rapids/fillmein/g settings.ini
+sed -i s/Rapids/Pytorch-Classification/g settings.ini
+nbdev_build_lib
+sed -i s/Pytorch-Classification/fillmein/g settings.ini

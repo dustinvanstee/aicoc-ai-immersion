cfa50627a3d161239dffb18157a6e053f133db50
Dustin VanStee <vanstee@login5.summit.olcf.ornl.gov>
2020-09-28 15:53:46 -0400

    added Tabular to process_files

--- process_files.sh
+++ process_files.sh
@@ -2,4 +2,6 @@ sed -i s/fillmein/Rapids/g settings.ini
 nbdev_build_lib
 sed -i s/Rapids/Pytorch-Classification/g settings.ini
 nbdev_build_lib
-sed -i s/Pytorch-Classification/fillmein/g settings.ini
+sed -i s/Pytorch-Classification/Tabular/g settings.ini
+nbdev_build_lib
+sed -i s/Tabular/fillmein/g settings.ini

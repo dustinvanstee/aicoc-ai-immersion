e8dfb09483da65ff4ac49305f26fd7f1dbcf617e
Dustin <dustinvanstee@gmail.com>
2020-09-28 14:25:52 -0400

    Update README.sh

--- FastAI/README.sh
+++ FastAI/README.sh
@@ -1,4 +1,4 @@
 ## FastAI Notebooks
 
 These notebooks were tested under WML-CE 1.7 .  They are FastAI version 1.0 notebooks.   
-Most notebooks here are not original and provided by the FastAI course-v3 repo.   
+Inspiration for the notebooks were from the FastAI course-v3 repo.   

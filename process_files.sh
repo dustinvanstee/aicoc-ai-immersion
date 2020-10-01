0927384c22e8be9b19fe4f2e4b166f2a9c329a41
Robert Chesebrough <racheseb@login3.summit.olcf.ornl.gov>
2020-09-30 22:23:24 -0400

    add exports to generate py all the way thru inference at end

--- process_files.sh
+++ process_files.sh
@@ -4,4 +4,6 @@ sed -i s/Rapids/Pytorch-Classification/g settings.ini
 nbdev_build_lib
 sed -i s/Pytorch-Classification/Tabular/g settings.ini
 nbdev_build_lib
-sed -i s/Tabular/fillmein/g settings.ini
+sed -i s/Tabular/UniversalFunctionApproximator/g settings.ini
+nbdev_build_lib
+sed -i s/UniversalFunctionApproximator/fillmein/g settings.ini
